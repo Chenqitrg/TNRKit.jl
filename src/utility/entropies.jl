52ea9d7da2b2864f595a1e08019fc721d6db178b
Chenqi Meng <mcqgravity@gmail.com>
2026-02-20 00:46:25 +0800

    Update entropies.jl

--- src/utility/entropies.jl
+++ src/utility/entropies.jl
@@ -1,6 +1,7 @@
-function VN_entropy(M::TensorMap)
+function VN_entropy(M::TensorMap; rtol = 1e-14, power = 1.0)
     _, S, _ = svd_trunc(M)
-    S_vec_norm = S.data / S.data[1]
-    S_von = - sum(S_vec_norm .* log.(S_vec_norm))
+    S_vec_norm = S.data / sum(S.data)
+    plogp = map(x -> x > rtol ? x^power * power * log(x) : 0.0, S_vec_norm)
+    S_von = - sum(plogp)
     return S_von
 end
