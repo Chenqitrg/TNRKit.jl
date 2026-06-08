fa03f3fe592e46453c5280ad62102dcce060da4f
Yue Zhengyuan <yuezy1997@icloud.com>
2026-06-08 15:45:17 +0800

    Anisotropic 2D Ising model (#184)
    * Anisotropic 2D Ising model
    
    * Try fixing fast tests
    
    * Clean up notations and tests
    
    * More docstring fix

--- src/TNRKit.jl
+++ src/TNRKit.jl
@@ -10,6 +10,7 @@ using DocStringExtensions
 using SpecialFunctions
 using FastGaussQuadrature
 using QuadGK
+using Roots
 using Base.Threads
 using Combinatorics: permutations
 
@@ -93,7 +94,8 @@ include("models/ising.jl")
 include("models/ising_triangular.jl")
 include("models/ising_honeycomb.jl")
 export classical_ising, ising_βc, f_onsager, ising_cft_exact,
-    ising_βc_3D, classical_ising_3D, ising_3D_free_energy_htse, classical_ising_impurity,
+    ising_βc_3D, classical_ising_3D, ising_3D_free_energy_htse,
+    classical_ising_impurity, ising_anisotropic_βc, f_onsager_anisotropic,
     classical_ising_triangular, ising_βc_triangular, f_onsager_triangular,
     classical_ising_honeycomb, ising_βc_honeycomb, f_onsager_honeycomb
 
